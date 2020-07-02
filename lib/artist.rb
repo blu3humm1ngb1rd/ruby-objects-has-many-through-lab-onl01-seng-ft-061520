e502611a96e72f58b802f4c0131db7e0915a3a7e
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-02 02:21:27 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist 
+  
+  attr_accessor :name 
+
+  @@all = []
+  
+  def initialize(name) 
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_song(song, artist, genre)
+    Song.new(song, self, genre)
+  end 
+  
+  def songs 
+    Song.all.select do |song|
+      song.artist == self 
+    end 
+  end 
+  
+  def genres 
+    Song.all.select do |song|
+      if song.artist == self 
+       song.genre 
+     end 
+   end 
+  end 
+  
+end 
